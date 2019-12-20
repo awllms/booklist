e3d511d786a24a610ddee38feba357bd7a3b3950
Anthony Williams <antcodess@gmail.com>
2019-12-20 08:16:39 -0800

    Modifies Header, ImageSlider and cart Icon component styles

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -3,6 +3,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+import photoOne from '../../assets/photo1.jpg';
+import photoTwo from '../../assets/photo2.jpg';
+import photoThree from '../../assets/photo3.jpg';
 import './ImageSlider.scss';
 
 const ImageSlider = () => {
@@ -21,9 +24,15 @@ const ImageSlider = () => {
   return (
     <section className='slider'>
       <Slider {...settings } >
-        <div className='image-placeholder'></div>
-        <div className='image-placeholder'></div>
-        <div className='image-placeholder'></div>
+        <div className='image-placeholder'>
+          <img src={photoOne} alt='Books' />
+        </div>
+        <div className='image-placeholder'>
+          <img src={photoTwo} alt='Books' />
+        </div>
+        <div className='image-placeholder'>
+          <img src={photoThree} alt='Books' />
+        </div>
       </Slider>
     </section>
   );
