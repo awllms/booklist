cc6f048a0c0ae147fbb835cff7f97b57211a86c2
Anthony Williams <antcodess@gmail.com>
2020-01-28 11:25:34 -0800

    Removes testing console in firebase utils

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -77,7 +77,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!snapShot.exists) {
     const {displayName, email } = userAuth;
     const createdAt = new Date();
-    console.log(userRef)
 
     try {
       await userRef.set({
