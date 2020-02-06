0de932ddcf296b011e60cf391948a3bd7422343f
Anthony Williams <antcodess@gmail.com>
2020-02-06 08:18:36 -0800

    Updates test stripe card exp date

--- client/src/pages/CheckoutPage/CheckoutPage.js
+++ client/src/pages/CheckoutPage/CheckoutPage.js
@@ -3,7 +3,10 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
-import { selectCartItems, selectCheckoutTotal } from '../../redux/cart/cart.selectors';
+import {
+  selectCartItems,
+  selectCheckoutTotal
+} from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { setAlert } from '../../redux/alert/alert.actions';
@@ -15,69 +18,96 @@ import StripeCheckoutButton from '../../components/StripeButton/StripeButton';
 
 import './CheckoutPage.scss';
 
-export const CheckoutPage = ({ cartItems, total, currentUser, setAlert, history }) => {
+export const CheckoutPage = ({
+  cartItems,
+  total,
+  currentUser,
+  setAlert,
+  history
+}) => {
   return (
     <React.Fragment>
-      <BreadCrumbNav title='Checkout'/>
-      <section className='checkout-page-container'>
+      <BreadCrumbNav title="Checkout" />
+      <section className="checkout-page-container">
         <h2>Checkout</h2>
-        <div className='checkout-page'>
-          <div className='checkout-header'>
-            <div className='header-block'>
+        <div className="checkout-page">
+          <div className="checkout-header">
+            <div className="header-block">
               <span>Product</span>
             </div>
-            <div className='header-block'>
+            <div className="header-block">
               <span>Title</span>
             </div>
-            <div className='header-block'>
+            <div className="header-block">
               <span>Quantity</span>
             </div>
-            <div className='header-block'>
+            <div className="header-block">
               <span>Price</span>
             </div>
-            <div className='header-block'>
+            <div className="header-block">
               <span>Remove</span>
             </div>
           </div>
-          { cartItems.length ? 
-              cartItems.map(item => <CheckoutItem key={item.id} item={item} />)
-              : (<div className='checkout-message'><span>Your cart is empty.</span></div>)
-          }
-          <div className='total'>
-            <span className='total-bold'>Total: <span>${total}</span></span>
+          {cartItems.length ? (
+            cartItems.map(item => <CheckoutItem key={item.id} item={item} />)
+          ) : (
+            <div className="checkout-message">
+              <span>Your cart is empty.</span>
+            </div>
+          )}
+          <div className="total">
+            <span className="total-bold">
+              Total: <span>${total}</span>
+            </span>
           </div>
-          <div className='test-warning'>
-            <span>This app was created for testing/proof of concept purposes.</span>
-            <span>*Please ONLY use the following test credit card for payments*</span>
-            <span>4242 4242 4242 4242 - Exp: 01/20 - CVV: 123</span>
+          <div className="test-warning">
+            <span>
+              This app was created for testing/proof of concept purposes.
+            </span>
+            <span>
+              *Please ONLY use the following test credit card for payments*
+            </span>
+            <span>4242 4242 4242 4242 - Exp: 01/22 - CVV: 123</span>
           </div>
-          <div className='checkout-button'>
-            { cartItems.length ?
-              currentUser ?
-              <StripeCheckoutButton price={total} cartItems={cartItems} currentUser={currentUser} />
-              : (<CustomButton 
+          <div className="checkout-button">
+            {cartItems.length ? (
+              currentUser ? (
+                <StripeCheckoutButton
+                  price={total}
+                  cartItems={cartItems}
+                  currentUser={currentUser}
+                />
+              ) : (
+                <CustomButton
                   onClick={() => {
                     setAlert({
                       status: 'regular',
                       message: 'Please sign in to complete checkout.'
                     });
                     history.push('/signin');
-                  }}>Pay Now</CustomButton>
-                )
-              : (<CustomButton 
-                  onClick={() => {
-                    setAlert({
-                      status: 'failure',
-                      message: 'Your cart is empty. Please add an item to complete checkout process.'
-                    });
-                  }}>Pay Now</CustomButton>
-                )
-            }
+                  }}
+                >
+                  Pay Now
+                </CustomButton>
+              )
+            ) : (
+              <CustomButton
+                onClick={() => {
+                  setAlert({
+                    status: 'failure',
+                    message:
+                      'Your cart is empty. Please add an item to complete checkout process.'
+                  });
+                }}
+              >
+                Pay Now
+              </CustomButton>
+            )}
           </div>
         </div>
       </section>
     </React.Fragment>
-  )
+  );
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -88,6 +118,9 @@ const mapStateToProps = createStructuredSelector({
 
 export const mapDispatchToProps = dispatch => ({
   setAlert: alert => dispatch(setAlert(alert))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(CheckoutPage));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(CheckoutPage));
