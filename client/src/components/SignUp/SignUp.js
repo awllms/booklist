9d6a608ad93b9d4f5dff05c8902fb4a05e5d8f7a
Anthony Williams <antcodess@gmail.com>
2020-02-13 21:34:05 -0800

    Fixes SignUp component css and adds more feedback on signin buttons hover state

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -10,19 +10,18 @@ import { setAlert } from '../../redux/alert/alert.actions';
 import './SignUp.scss';
 
 export const SignUp = ({ signUpStart, setAlert }) => {
-  
   const [userCredentials, setUserCredentials] = useState({
     displayName: '',
     email: '',
     password: '',
     confirmPassword: ''
   });
-  
+
   const { displayName, email, password, confirmPassword } = userCredentials;
-  
-  const onInputSubmit = (event) => {
+
+  const onInputSubmit = event => {
     event.preventDefault();
-  
+
     if (password !== confirmPassword) {
       setAlert({
         status: 'failure',
@@ -32,57 +31,59 @@ export const SignUp = ({ signUpStart, setAlert }) => {
     }
     signUpStart({ email, password, displayName });
   };
-  
-  const onInputChange = (event) => {
+
+  const onInputChange = event => {
     const { name, value } = event.target;
-    setUserCredentials({...userCredentials, [name]: value })
+    setUserCredentials({ ...userCredentials, [name]: value });
   };
 
   return (
-    <section className='sign-up'>
+    <section className="sign-up">
       <h2>SignUp</h2>
       <h3>I do not have an account.</h3>
       <span>Sign up with your email and password</span>
       <form onSubmit={onInputSubmit}>
-      <FormInput 
-          name='displayName'
-          type='text'
+        <FormInput
+          name="displayName"
+          type="text"
           value={displayName}
-          onChange={onInputChange} 
-          label='Name'
-          placeholder='Name' 
-          required 
+          onChange={onInputChange}
+          label="Name"
+          placeholder="Name"
+          required
         />
-      <FormInput 
-          name='email'
-          type='email'
+        <FormInput
+          name="email"
+          type="email"
           value={email}
-          onChange={onInputChange} 
-          label='Email'
-          placeholder='Email' 
-          required 
+          onChange={onInputChange}
+          label="Email"
+          placeholder="Email"
+          required
         />
-        <FormInput 
-          name='password'
-          type='password'
+        <FormInput
+          name="password"
+          type="password"
           value={password}
-          onChange={onInputChange} 
-          label='Password'
-          placeholder='Password' 
-          required 
+          onChange={onInputChange}
+          label="Password"
+          placeholder="Password"
+          required
         />
-        <FormInput 
-          name='confirmPassword'
-          type='password'
+        <FormInput
+          name="confirmPassword"
+          type="password"
           value={confirmPassword}
-          onChange={onInputChange} 
-          label='Confirm Password'
-          placeholder='Confirm Password' 
-          required 
+          onChange={onInputChange}
+          label="Confirm Password"
+          placeholder="Confirm Password"
+          required
         />
-        <CustomButton 
-        type='submit'
-        isRegularSignIn>Sign Up</CustomButton>
+        <div className="button-signup">
+          <CustomButton type="submit" isRegularSignIn>
+            Sign Up
+          </CustomButton>
+        </div>
       </form>
     </section>
   );
